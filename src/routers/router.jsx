4e9123caedc274c64c6332cb59779db03818187d
Alessandro <alessandroRetuerto@hotmail.com>
2023-12-26 20:44:16 -0500

    change to createHashRouter

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createHashRouter } from "react-router-dom";
 import { HomePage } from "../page/HomePage";
 import { Register } from "../page/Register";
 import { Login } from "../page/Login";
 import { ProtectecRouter } from "./ProtectecRouter";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/register",
     element: <Register></Register>,
